36129207dbc083473bec573b5d85f9ebf27be39f
Rayne <rayne.r.binneman@gmail.com>
2025-05-23 14:27:24 +0200

    Added to AttributesCreator Page. Created an Add Attribute Group Dialog

---
+++ CharacterSheetCreator/CharacterSheetCreator/Features/AttributeCreation/Models/AttributesDto.cs
@@ -0,0 +1,16 @@
+namespace CharacterSheetCreator.Features.AttributeCreation.Models;
+
+public class AttributeGroupsDto
+{
+    public int AttributeGroupId { get; set; }
+    public string? AttributeGroupName { get; set; } = string.Empty;
+    public List<AttributeDto?> Attributes { get; set; } = new List<AttributeDto?>();
+    
+}
+
+public class AttributeDto
+{
+    public int AttributeId { get; set; }
+    public string? AttributeName { get; set; } = string.Empty;
+    public int AttributeGroupId { get; set; }
+}
